f5de29070793dbe1d9d61fc14f2a099cd2122557
dziuplabanino <dziupla.banino@gmail.com>
2025-10-19 22:39:51 +0200

    add api endpoint generateSign

---
+++ pages/api/generateSign.js
@@ -0,0 +1,27 @@
+import crypto from "crypto";
+
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Only POST method allowed" });
+  }
+
+  const { sessionId, amount, currency, env } = req.body;
+
+  const merchantId = "354420"; // stała wartość
+  const crc = "1234567890abcdef"; // <- zmień na swój CRC z p24
+  const data = {
+    sessionId,
+    merchantId,
+    amount,
+    currency,
+    crc,
+  };
+
+  // Utwórz string do podpisania
+  const composeString = JSON.stringify(data, Object.keys(data).sort());
+
+  // Wybierz algorytm (SHA384 lub SHA512)
+  const hash = crypto.createHash("sha384").update(composeString).digest("hex");
+
+  return res.status(200).json({ sign: hash });
+}
